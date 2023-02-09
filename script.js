275b63f3db1f367b42be2e9d148c20b40c0ae3b1
Ammar Hasan <ammarhasan011@gmail.com>
2023-02-09 23:14:10 +0100

    fixade formen

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const loginBtn= document.querySelector(".loginBtn");
 const form = document.querySelector(".form");
 const xBtn= document.querySelector(".xBtn");
 const logoutBtn = document.querySelector(".logoutBtn");
-const h2welcome = document.querySelector(".welcome");
+const firstMessage = document.querySelector(".firstMessage");
+const secondMessage = document.querySelector(".secondMessage");
+const bgimagetext = document.querySelector(".bgimagetext");
+
+
 let username =document.querySelector(".username");
 
 
@@ -14,6 +18,8 @@ xBtn.addEventListener("click", cancelForm);
 
 
 logoutBtn.style.display = "none";
+firstMessage.style.display = "none";
+secondMessage.style.display = "none";
 form.style.display="none";
 
 
@@ -38,18 +44,20 @@ init()
 function popUp(){
     console.log("popup form")
     form.style.display="block";
+    bgimagetext.style.display="none";
 }
 
 function cancelForm(){
     console.log("cancel form")
     form.style.display="none";
+    bgimagetext.style.display="block";
 }
 
 function checkCode(){
     let search= false;
     for (let users of userArray){
-        console.log(users.username);
-        console.log(username.value);
+        // console.log(users.username);
+        // console.log(username.value);
      if (users.username == username.value) {
         search=true;
          localStorage.setItem("userName", username.value);
@@ -64,16 +72,24 @@ function checkCode(){
 
 //function for seccessful login
 function renderSeccessful(){
-    let userkey =localStorage.getItem("userName", username.value);
-    if(username){
-        h2welcome.innerText = "Välkommen Tillbaka" + " " + userkey;
-    }else {
-        h2welcome.innerText = "DU ÄR INLOGGAD SOM" + " " + userkey;
+    let userkey = localStorage.getItem("userName");
+    // let userkey = localStorage.getItem("userName", username.value);
+    
+    if (!username.value == userkey){
+          //DU ÄR INLOGGAD SOM
+        let loggingIn = document.querySelector(".loggingIn");
+        loggingIn.innerText = userkey;
+        secondMessage.style.display = "block";
+    } else {
+         //Välkommen tillbaka
+        let loggedIn = document.querySelector(".loggedIn");
+        loggedIn.innerText = userkey;
+        firstMessage.style.display = "block";
     }
     form.style.display = "none";
     popupBtn.style.display="none";
     logoutBtn.style.display="block";
-
+    bgimagetext.style.display="block";
 }
 
 function logOut(){
@@ -81,5 +97,8 @@ function logOut(){
     form.style.display="none";
     logoutBtn.style.display = "none";
     popupBtn.style.display="block";
-    // h2welcome.style.display = "block";
+    firstMessage.style.display = "none";
+    secondMessage.style.display = "none";
+    username.value = "";
+    bgimagetext.style.display="block";
 }
